9754873bff8cebb1cb72bfd351b4b431652cc958
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2014-08-25 11:03:05 +0200

    fix/ajout test parametre status is_a numeric suppression des puts du retour de reponse, et ajout de prisition sur les msg d'erreurs

--- lib/shipyard-cli.rb
+++ lib/shipyard-cli.rb
@@ -5,35 +5,29 @@ module ShipyardCli
 
   def url(server_url = nil)
     unless server_url.include?('://')
-      puts "Set Global var on your code '$URL_SHIYARD_CLI' with valid url"
+      puts "ShipyardCli.deploy() Set Global var on your code '$URL_SHIYARD_CLI' with valid url"
       return
     end
     @server ||= server_url
   end
 
   def deploy(opts = {})
-
-    if !opts[:application] || !opts[:environment] || !opts[:status] || opts[:status] === 99999
-      puts "ShiptardCli.deploy() need 3 args: application, environment, status, [commit_hash]"
+    if !opts[:application] || !opts[:environment] || !opts[:status] || !opts[:status].is_a?(Numeric)
+      puts "ShiptardCli.deploy() need 3 args: application, environment, status(numeric), [commit_hash]"
       return
     end
-
     deploy = {
       :application => opts[:application],
       :environment => opts[:environment],
       :status => opts[:status],
       :commit_hash => opts[:commit_hash]
     }
-
     uri = URI(@server) # Global Var Set by User on his code 'http://api.xxx.zz/v1/deployments'
     http = Net::HTTP.new(uri.host, uri.port)
     req = Net::HTTP::Post.new(uri.path)
     req.add_field "Content-Type", "Application/json"
     req.body = deploy.to_json
     response = http.request(req)
-
-    puts "Status: #{response.code} #{response.message} Response body following..."
-    puts response.body
   end
   
   extend self
