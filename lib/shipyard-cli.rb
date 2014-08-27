f27ca2d5fd2970aeeb463568077af2128dff78c5
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2014-08-27 11:36:23 +0200

    gem operationnel avec convention gem; shipyard-cli autonome a l'installation

---
+++ lib/shipyard-cli.rb
@@ -0,0 +1,47 @@
+require 'json'
+require 'net/http'
+require 'capistrano/all'
+require 'awesome_print'
+
+module ShipyardCli
+  def initialize
+    ap '---------------------- DEBUGG initialize !!! ----------------------'
+  end
+
+  def record(status)
+    ap '---------------------- DEBUGG record !!! ----------------------'
+    options = {:application => fetch(:application), :environment => fetch(:stage), :status => status, :commit_hash => `git rev-parse HEAD && date`}
+    self.deploy(options)
+  end
+
+  def url(server_url = nil)
+    ap '---------------------- DEBUGG url !!! ----------------------'
+    unless server_url.include?('://')
+      puts "ShipyardCli.deploy() Set Global var on your code '$URL_SHIYARD_CLI' with valid url"
+      return
+    end
+    @server ||= server_url
+  end
+
+  def deploy(opts = {})
+    ap '---------------------- DEBUGG deploy !!! ----------------------'
+    if !opts[:application] || !opts[:environment] || !opts[:status] || !opts[:status].is_a?(Numeric)
+      puts "ShiptardCli.deploy() need 3 args: application, environment, status(numeric), [commit_hash]"
+      return
+    end
+    deploy = {
+      :application => opts[:application],
+      :environment => opts[:environment],
+      :status => opts[:status],
+      :commit_hash => opts[:commit_hash]
+    }
+    uri = URI(@server) # Global Var Set by User on his code 'http://api.xxx.zz/v1/deployments'
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.path)
+    req.add_field "Content-Type", "Application/json"
+    req.body = deploy.to_json
+    response = http.request(req)
+  end
+  
+  extend self
+end
